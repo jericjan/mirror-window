ca0de4af9afb38dc999d182fb716b2ac6770b0e0
jericjan <cantilfrederick@gmail.com>
2023-06-17 18:55:10 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+from screenshot import screenshot
+
+# create a window to display the mirror image
+cv2.namedWindow("Mirror", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("Mirror", cv2.WND_PROP_TOPMOST, 1)
+# loop over the frames
+while True:
+    # capture a screenshot of the screen
+    shot = screenshot("NIKKE")
+    if shot is None:
+        print(
+            "Can't find the window. Check if the name is correct or try running as admin."
+        )
+        break
+    # convert the screenshot to a NumPy array
+    screenshot_array = cv2.cvtColor(np.array(shot), cv2.COLOR_RGB2BGR)
+    # screenshot_array = cv2.cvtColor(np.array(shot))
+
+    # display the mirror image in the window
+    cv2.imshow("Mirror", screenshot_array)
+
+    # wait for a key press to exit the program
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# release the window and exit the program
+cv2.destroyAllWindows()
---
+++ screenshot.py
@@ -0,0 +1,49 @@
+from ctypes import windll
+
+import win32gui
+import win32ui
+from PIL import Image
+
+
+def screenshot(window_name):
+    hwnd = win32gui.FindWindow(None, window_name)
+    print(f"Window ID: {hwnd}", end=", ")
+    class_name = win32gui.GetClassName(hwnd)
+    print(f"Class name: {class_name}", end=", ")
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    # left, top, right, bot = win32gui.GetClientRect(hwnd)
+    left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w = right - left
+    h = bot - top
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area.
+    # result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 1)
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 2)
+    print(f"PrintWindow result: {result}")
+
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        "RGB", (bmpinfo["bmWidth"], bmpinfo["bmHeight"]), bmpstr, "raw", "BGRX", 0, 1
+    )
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        return im
